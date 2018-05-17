79d4200a466215fa2cabeb700970a550aca3184d
Hossam Elsagheer <hossam.elsagheer@gmail.com>
2018-05-17 04:32:34 +0200

    First Commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp=require('gulp');
+const sass=require('gulp-sass');
+const browserSync=require('browser-Sync');
+const autoprefixer=require('gulp-autoprefixer');
+
+gulp.task('sass',function(){
+  return gulp.
+  gulp(sass())
+  gulp.src('/src/scss/*.scss')
+  .gulp.pipe()
+
+});
